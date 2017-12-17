f76a12a0f9e666997467c8e4ffb3f999126fdd12
Sven Seemann <sven.seemann@bindoc.de>
2017-12-17 15:13:27 +0100

    updated publish script

--- publish.sh
+++ publish.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-PACKAGE_VERSION=$(cat package.json \
+CURRENT_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
+echo "Current Version: $CURRENT_VERSION"
+echo "Type the version that you want to set (X.X.X format), followed by [ENTER]:"
+read NEXT_VERSION
+
+# STEP NPM
+echo "Setting package.json version:     $NEXT_VERSION"
+npm version ${NEXT_VERSION}
+
+# STEP BUILD
+set -e  # terminates on error
 npm run build
+node publish_index_remove.js  # replace all occurrences of [/index] in 'dist/'
+set +e
 
-# replace all occurrences of [/index] in 'dist/'
-node publish_index_remove.js
-
-git tag $PACKAGE_VERSION
-git push
-git push --tags
+# STEP PUBLISH
+echo "Publish version:                  $NEXT_VERSION"
+git push --follow-tags
 
 cd dist/
-
-echo
-echo "Files, published now ..."
-echo
-ls
 npm publish
