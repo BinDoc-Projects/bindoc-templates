aaba728bcc95b15b50aaa9c2494280f07eb3e59c
Sven Seemann <sven.seemann@bindoc.de>
2017-12-18 13:56:37 +0100

    BdTempalteProvider.templates is now optional

--- src/lib/src/dynamic/template-provider.model.ts
+++ src/lib/src/dynamic/template-provider.model.ts
@@ -2,7 +2,8 @@ import {Type} from "@angular/core";
 import {BdTemplate} from "./abstract-template.model";
 
 export interface BdTemplateProvider {
-  templates: Type<BdTemplate>[]
+
+  templates?: Type<BdTemplate>[]
 
   getTemplates(type: string): Type<BdTemplate>;
 }
